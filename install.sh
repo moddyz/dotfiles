61094e20eb89025c9a3d16a82037b315f0cafb6d
moddyz <moddy3d@gmail.com>
2020-02-09 15:01:30 +1300

    Added .gitconfig

--- install.sh
+++ install.sh
@@ -7,4 +7,5 @@ cp -v $(dirname "$0")/.vimrc ~/.vimrc
 mkdir -v -p ~/.vim/plugin/
 cp -v $(dirname "$0")/plugin/* ~/.vim/plugin/
 cp -v $(dirname "$0")/.bash_aliases ~/.bash_aliases
+cp -v $(dirname "$0")/.gitconfig ~/.gitconfig
 echo "Done!"
