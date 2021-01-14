c2afea7f7a694dfec3cd4ffbc849e0a044b3ed58
moddyz <moddy3d@gmail.com>
2021-01-14 23:37:30 +1300

    Added .tmux.conf

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ cp -v $(dirname "$0")/.vimrc ~/.vimrc
 mkdir -v -p ~/.vim/plugin/
 cp -r -v $(dirname "$0")/.vim ~/
 cp -v $(dirname "$0")/.bash_aliases ~/.bash_aliases
-cp -v $(dirname "$0")/.gitconfig ~/.gitconfig
+cp -v $(dirname "$0")/.tmux.conf ~/.tmux.conf
 echo "Done!"
