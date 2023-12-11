ead45aa9d1745cbcefc671cca0cb238fee2d4ff3
moddyz <moddy3d@gmail.com>
2023-12-12 12:34:40 +1300

    Also install .gitconfig

--- install.sh
+++ install.sh
@@ -2,16 +2,17 @@
 
 set -euxo pipefail
 
-echo "Installing linux configuration files."
+echo "Installing files for Linux & MacOS."
 cp -v $(dirname "$0")/.vimrc ~/.vimrc
 mkdir -v -p ~/.vim/plugin/
 cp -r -v $(dirname "$0")/.vim ~/
 cp -v $(dirname "$0")/.zsh_aliases ~/.zsh_aliases
 cp -v $(dirname "$0")/.tmux.conf ~/.tmux.conf
 cp -v $(dirname "$0")/.gdbinit ~/.gdbinit
+cp -v $(dirname "$0")/.gitconfig ~/.gitconfig
 
 if [[ $OSTYPE == 'darwin'* ]]; then
-    echo "Mac OSX detected."
+    echo "Installing Mac OSX specific configuration"
     mkdir -v -p ~/.config/karabiner/
     cp -v $(dirname "$0")/.config/karabiner/karabiner.json ~/.config/karabiner
 fi
